7ddbdab08ea8c5b228d97c0520768dedb500d32e
khaliev <dev.adlan@icloud.com>
2023-06-19 11:53:32 +0200

    express quest 2 finished

---
+++ userHandlers.js
@@ -0,0 +1,36 @@
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  database
+    .query("SELECT * FROM users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from the database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("SELECT * FROM users WHERE id = ?", [id])
+    .then(([user]) => {
+      if (users.length > 0) {
+        res.status(200).json(user[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from the database");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
